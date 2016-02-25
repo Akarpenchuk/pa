487d2ad66259f86bba3be42d6b8ec64c4ee837b2
Dmitriy Malovaniy <d.malovaniy@modnakasta.ua>
2016-02-25 09:11:33 +0200

    start_to_LandingLogin

---
+++ dat/landing_page/landing_elements.py
@@ -0,0 +1,21 @@
+AUTH_LINK = "//a[@id='login-show']"
+AUTH_FORM = "//form[@id='login-form']"
+AUTH_EMAIL_INPUT = "//form[@id='login-form']/div/input[@name='username']"
+AUTH_PASS_INPUT = "//form[@id='login-form']/div/input[@name='password']"
+AUTH_BTN = "//a[@id='login-button']"
+
+REG_LINK = "//"
+REG_FORM = "//"
+REG_EMAIL_INPUT = "//"
+REG_PASS_INPUT = "//"
+REG_BTN = "//"
+REG_ICO = "//"
+
+
+AUTH_FB = "//"
+AUTH_GMAIL = "//"
+AUTH_MAILRU = "//"
+
+RECOVERY_FORM = "//"
+SEND_AGAIN = "//"
+COMMENTS = "//"
---
+++ testLandingLogin.py
@@ -0,0 +1,40 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import sys, os
+sys.path.append('/home/ace/Documents/git/autotests/dat')
+
+import unittest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+
+from base_methods.base import BaseClass
+from dat.landing_page.landing import Landing
+
+import dat.landing_page.landing_elements as le
+
+
+class Test(unittest.TestCase, BaseClass, Landing):
+
+    def setUp(self):
+
+        chromeOptions = Options()
+        chromeOptions.add_argument("--start-maximized")
+        self.driver = webdriver.Chrome(chrome_options=chromeOptions)
+        self.wait = WebDriverWait(self.driver, 10)
+    
+    def testLandingLogin(self):
+
+        self.open_url("https://modnakasta.ua/landing/nike", le.AUTH_FORM)
+        self.landing_login()
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+
+if __name__=="__main__":
+    print "running"
+    unittest.main()
